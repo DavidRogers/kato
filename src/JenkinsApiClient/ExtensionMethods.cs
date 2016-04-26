45500acff3c82ceefc446293ce5ae928415ef4f6
ali.ozgur <ali.ozgur@bilgi.edu.tr>
2016-04-26 15:26:38 +0300

    Implement authentication using Windows Credentials Management API
    * Store credentials in Windows Credentials Manager
    * Add Select All, Clear and Toggle functions for Job selection
    * Load user settings properly on app starting

---
+++ src/JenkinsApiClient/ExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace JenkinsApiClient
+{
+    public static class ExtensionMethods
+    {
+        public static void ApplyCredentials(this HttpClient client, UserCredentials credentials)
+        {
+            if (credentials == null || String.IsNullOrWhiteSpace(credentials.UserName))
+                return;
+
+            var byteArray = Encoding.UTF8.GetBytes(String.Format("{0}:{1}", credentials.UserName, credentials.Password));
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+        }
+    }
+}
--- src/JenkinsApiClient/HttpHelper.cs
+++ src/JenkinsApiClient/HttpHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -9,30 +10,85 @@ namespace JenkinsApiClient
 {
 	public static class HttpHelper
 	{
-		public static Task<string> GetJsonAsync(Uri path)
+
+		#region Credentials local in memory cache
+
+		private static Dictionary<string, UserCredentials> _cachedCredentials = new Dictionary<string, UserCredentials>();
+
+		private static UserCredentials CachedCreds_TryGet(string baseAddress)
+		{
+			if (String.IsNullOrWhiteSpace(baseAddress))
+				return null;
+
+			var lowerAddress = baseAddress.ToLower();
+			return _cachedCredentials.ContainsKey(lowerAddress) ? _cachedCredentials[lowerAddress] : null;
+		}
+
+		private static void CachedCreds_Set(string baseAddress, UserCredentials creds)
+		{
+			if (String.IsNullOrWhiteSpace(baseAddress) || creds == null || !creds.IsValid)
+				return;
+
+			var lowerAddress = baseAddress.ToLower();
+			if (_cachedCredentials.ContainsKey(lowerAddress))
+				return;
+
+			_cachedCredentials.Add(lowerAddress, creds);
+
+		}
+
+		private static bool CachedCreds_Contains(string baseAddress)
+		{
+			if (String.IsNullOrWhiteSpace(baseAddress))
+				return false;
+
+			var lowerAddress = baseAddress.ToLower();
+			return _cachedCredentials.ContainsKey(lowerAddress);
+		}
+
+		private static UserCredentials EnsureCredentials(string baseAddress, UserCredentials credentials)
+		{
+			if (credentials != null)
+				CachedCreds_Set(baseAddress, credentials);
+
+			return credentials != null ? credentials : CachedCreds_TryGet(baseAddress);
+		}
+		#endregion
+
+
+		public static Task<string> GetJsonAsync(Uri path, UserCredentials credential = null)
 		{
 			using (HttpClient client = new HttpClient { BaseAddress = new Uri(path.Scheme + "://" + path.Host + ":" + path.Port) })
 			{
+
+				var cred = EnsureCredentials(client.BaseAddress.ToString(), credential);
+				client.ApplyCredentials(cred);
+
 				HttpResponseMessage result = client.GetAsync(path.PathAndQuery).Result;
 				result.EnsureSuccessStatusCode();
 				return result.Content.ReadAsStringAsync();
 			}
 		}
 
-		public static string GetJson(Uri path)
+		public static string GetJson(Uri path, UserCredentials credential = null)
 		{
 			using (HttpClient client = new HttpClient { BaseAddress = new Uri(path.Scheme + "://" + path.Host + ":" + path.Port) })
 			{
+
+				var cred = EnsureCredentials(client.BaseAddress.ToString(), credential);
+				client.ApplyCredentials(cred);
 				HttpResponseMessage result = client.GetAsync(path.PathAndQuery).Result;
 				result.EnsureSuccessStatusCode();
 				return result.Content.ReadAsStringAsync().Result;
 			}
 		}
 
-		public static ConsoleOutput GetConsoleOutput(Uri path, long offset)
+		public static ConsoleOutput GetConsoleOutput(Uri path, long offset, UserCredentials credential = null)
 		{
 			using (HttpClient client = new HttpClient { BaseAddress = new Uri(path.Scheme + "://" + path.Host + ":" + path.Port) })
 			{
+				var cred = EnsureCredentials(client.BaseAddress.ToString(), credential);
+				client.ApplyCredentials(cred);
 				HttpResponseMessage result = client.GetAsync(path.PathAndQuery + "logText/progressiveText?start=" + offset).Result;
 				result.EnsureSuccessStatusCode();
 
@@ -45,10 +101,12 @@ namespace JenkinsApiClient
 			}
 		}
 
-		public static Task<string> PostData(Uri path, string data = "")
+		public static Task<string> PostData(Uri path, string data = "", UserCredentials credential = null)
 		{
 			using (HttpClient client = new HttpClient { BaseAddress = new Uri(path.Scheme + "://" + path.Host + ":" + path.Port) })
 			{
+				var cred = EnsureCredentials(client.BaseAddress.ToString(), credential);
+				client.ApplyCredentials(cred);
 				HttpResponseMessage result = client.PostAsync(path.PathAndQuery, new StringContent(data)).Result;
 				result.EnsureSuccessStatusCode();
 				return result.Content.ReadAsStringAsync();
